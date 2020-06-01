319559b45e339f12ab7e955ccff10fd9fd8bb11b
Hansel Ke <hansel@openb.net>
2020-06-01 12:50:56 +0800

    dev

--- iohookinstall.js
+++ iohookinstall.js
@@ -1,29 +1,29 @@
-const path = require('path');
-const runtime = process.versions['electron'] ? 'electron' : 'node';
-const essential = runtime + '-v' + process.versions.modules + '-' + process.platform + '-' + process.arch;
-const modulePath = path.join(__dirname, 'builds', essential, 'build', 'Release', 'iohook.node');
-console.info('The path is:', modulePath);
+// const path = require('path');
+// const runtime = process.versions['electron'] ? 'electron' : 'node';
+// const essential = runtime + '-v' + process.versions.modules + '-' + process.platform + '-' + process.arch;
+// const modulePath = path.join(__dirname, 'builds', essential, 'build', 'Release', 'iohook.node');
+// console.info('The path is:', modulePath);
 
-// var AutoLaunch = require('auto-launch');
+var AutoLaunch = require('auto-launch');
  
-// var minecraftAutoLauncher = new AutoLaunch({
-//     name: 'Local Tracker',
-//     path: '/Users/hanselke/.nvm/versions/node/v10.15.0/bin/localTracker',
-// });
+var localTrackerLauncher = new AutoLaunch({
+    name: 'Local Tracker',
+    path: 'localTracker',
+});
  
-// minecraftAutoLauncher.enable();
+localTrackerLauncher.enable();
  
-// //minecraftAutoLauncher.disable();
+//localTrackerLauncher.disable();
  
  
-// minecraftAutoLauncher.isEnabled()
-// .then(function(isEnabled){
-//     if(isEnabled){
-//         return;
-//     }
-//     minecraftAutoLauncher.enable();
-// })
-// .catch(function(err){
-//     // handle error
-//     console.error("autolauncher error",err)
-// });
+localTrackerLauncher.isEnabled()
+.then(function(isEnabled){
+    if(isEnabled){
+        return;
+    }
+    localTrackerLauncher.enable();
+})
+.catch(function(err){
+    // handle error
+    console.error("autolauncher error",err)
+});
