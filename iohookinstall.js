87536f3686b280987ee53ad24b5e9c08a4793802
Hansel Ke <hansel@openb.net>
2020-06-08 13:49:04 +0800

    0.0.11

--- iohookinstall.js
+++ iohookinstall.js
@@ -1,29 +1,48 @@
-const path = require('path');
-const runtime = process.versions['electron'] ? 'electron' : 'node';
-const essential = runtime + '-v' + process.versions.modules + '-' + process.platform + '-' + process.arch;
-const modulePath = path.join(__dirname, 'builds', essential, 'build', 'Release', 'iohook.node');
-console.info('The path is:', modulePath);
+// const path = require('path');
+// const runtime = process.versions['electron'] ? 'electron' : 'node';
+// const essential = runtime + '-v' + process.versions.modules + '-' + process.platform + '-' + process.arch;
+// const modulePath = path.join(__dirname, 'builds', essential, 'build', 'Release', 'iohook.node');
+// console.info('The path is:', modulePath);
+var which = require('which')
+ 
+var AutoLaunch = require('auto-launch');
 
-// var AutoLaunch = require('auto-launch');
+
+const trackerBinPath = which.sync('localTracker', {nothrow: true})
+
+if (trackerBinPath == null) {
+    return console.error("Cant find localTracker, need to install it first")
+}
+
+const nodeBinPath = which.sync('node', {nothrow: true})
+
+if (nodeBinPath == null) {
+    return console.error("Cant find node, need to install it first")
+}
+
+console.log("trackerBinPath",trackerBinPath)
+var autoLauncher = new AutoLaunch({
+    name: 'LocalTracker',
+    path: nodeBinPath,
+    extraArgs: trackerBinPath,
+    mac: {
+        useLaunchAgent: true
+    }
+});
  
-// var minecraftAutoLauncher = new AutoLaunch({
-//     name: 'Local Tracker',
-//     path: '/Users/hanselke/.nvm/versions/node/v10.15.0/bin/localTracker',
-// });
+autoLauncher.enable();
  
-// minecraftAutoLauncher.enable();
- 
-// //minecraftAutoLauncher.disable();
+//autoLauncher.disable();
  
  
-// minecraftAutoLauncher.isEnabled()
-// .then(function(isEnabled){
-//     if(isEnabled){
-//         return;
-//     }
-//     minecraftAutoLauncher.enable();
-// })
-// .catch(function(err){
-//     // handle error
-//     console.error("autolauncher error",err)
-// });
+autoLauncher.isEnabled()
+.then(function(isEnabled){
+    if(isEnabled){
+        return;
+    }
+    autoLauncher.enable();
+})
+.catch(function(err){
+    // handle error
+    console.error("autolauncher error",err)
+});
